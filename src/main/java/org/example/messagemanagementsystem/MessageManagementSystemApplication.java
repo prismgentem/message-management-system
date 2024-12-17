eccab7429cbfe1ebb835bc135464a783b1927729
prismgentem <foksfire12345px@gmail.com>
2024-12-17 19:54:35 +0300

    init

---
+++ src/main/java/org/example/messagemanagementsystem/MessageManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package org.example.messagemanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageManagementSystemApplication.class, args);
+    }
+
+}
